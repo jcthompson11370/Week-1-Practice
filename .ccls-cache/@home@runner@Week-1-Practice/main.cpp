1bba6971e270e188c53ab3525bcb6b1b924e2b71
johnthom11370 <>
2022-08-25 15:51:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Week-1-Practice/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int a = 2;
+  double b = 0;
+  
+  cout << "a =" << a << endl;
+  cout << "b =" << b << endl;
+
+  cout << "a/b = " << a/b << endl;
+
+  int d;
+  d = a/b;
+  cout << "int d = a/b = " << d << endl;
+
+  double g;
+  g = a/b; 
+  cout << "double g = a/b = " << g << endl;
+
+
+
+
+
+  
+  
+}
